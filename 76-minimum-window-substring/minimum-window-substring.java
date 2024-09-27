1ac88aac1f71d42d3bcfb24a3f7811cb72c661f5
Trey Hilton <89350594+trhilton94@users.noreply.github.com>
2024-09-27 02:02:19 -0400

    Time: 129 ms (11.44%) | Memory: 45.4 MB (30.43%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) return "";
+
+        Map<Character, Integer> windowMap = new HashMap<>();
+        Map<Character, Integer> needMap = new HashMap<>();
+
+        for (int i = 0; i < t.length(); i++) {
+            needMap.put(t.charAt(i), needMap.getOrDefault(t.charAt(i), 0) + 1);
+        } // for
+
+        int left = 0;
+        int need = needMap.entrySet().size();
+        int have = 0;
+        int minLength = Integer.MAX_VALUE;
+        String minWindow = "";
+        char rightChar;
+        char leftChar;
+
+        for (int right = 0; right < s.length(); right++) {
+            rightChar = s.charAt(right);
+            leftChar = s.charAt(left);
+            windowMap.put(rightChar, windowMap.getOrDefault(rightChar, 0) + 1);
+            if (needMap.containsKey(rightChar) &&
+                windowMap.get(rightChar).intValue() == needMap.get(rightChar).intValue()) have++;
+            while (left <= right && have == need) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    minWindow = s.substring(left, right + 1);
+                } // if
+                leftChar = s.charAt(left);
+                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+                if (needMap.containsKey(leftChar) &&
+                    windowMap.get(leftChar).intValue() < needMap.get(leftChar).intValue()) have--;
+                left++;
+            } // while
+        } // for
+
+        return minWindow;
+    } // minWindow
+} // Solution
