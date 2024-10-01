e9a01e0f9ea1a9e49bbcc763a79a6bd37aa6302e
Trey Hilton <89350594+trhilton94@users.noreply.github.com>
2024-10-01 18:57:26 -0400

    Time: 331 ms (89.46%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,4 @@
-SELECT Id
+SELECT w1.id
 FROM Weather w1
-WHERE temperature > (
-    SELECT temperature
-    FROM Weather w2
-    WHERE w2.recordDate = w1.recordDate - INTERVAL 1 DAY
-);
+JOIN Weather w2 ON DATE(w1.recordDate) = DATE(w2.recordDate) + INTERVAL 1 DAY
+WHERE w1.temperature > w2.temperature;
